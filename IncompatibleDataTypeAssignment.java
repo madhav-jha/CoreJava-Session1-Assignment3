f63f6155729894d8763e837a3081be5257d88e42
madhav-jha <madhavjha2008@gmail.com>
2016-07-17 00:04:02 +0530

    Add files via upload

---
+++ IncompatibleDataTypeAssignment.java
@@ -0,0 +1,13 @@
+package Session1.Assignment3;
+
+public class IncompatibleDataTypeAssignment {
+
+	public static void main(String[] args) {
+		int a,d;
+		long b=7;
+		double c=3.5;
+		a=b;
+		d=c;
+	}
+
+}
